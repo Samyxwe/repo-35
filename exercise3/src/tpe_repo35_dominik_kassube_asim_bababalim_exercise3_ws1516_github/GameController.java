fd2f552c809d6c7203754a93dd7fc520090363c5
a.bababalim <a.bababalim@141.19.145.21>
2015-10-26 15:09:02 +0100

    Update

--- exercise3/src/tpe_repo35_dominik_kassube_asim_bababalim_exercise3_ws1516_github/GameController.java
+++ exercise3/src/tpe_repo35_dominik_kassube_asim_bababalim_exercise3_ws1516_github/GameController.java
@@ -6,7 +6,18 @@ public class GameController {
 	Squad Spieler2;
 	
 	GameController(){
+		Spieler1 = new Squad();
+		Spieler2 = new Squad();
+	}
+	
+	void runGame(){
 		
 	}
 	
+	public static void main(String [] args){
+		
+		GameController Spiel=new GameController();
+		Spiel.runGame();
+		
+	}
 }
--- exercise3/src/tpe_repo35_dominik_kassube_asim_bababalim_exercise3_ws1516_github/Squad.java
+++ exercise3/src/tpe_repo35_dominik_kassube_asim_bababalim_exercise3_ws1516_github/Squad.java
@@ -1,7 +1,6 @@
 package tpe_repo35_dominik_kassube_asim_bababalim_exercise3_ws1516_github;
 
-import java.util.Scanner;
-
+import java.util.*;
 public class Squad {
 
 	Scanner in=new Scanner(System.in);
@@ -14,37 +13,128 @@ public class Squad {
 	Mensch mensch=new Mensch();
 	Untote untote=new Untote();
 	Ork ork=new Ork();
+	Nachtelf nachtelf=new Nachtelf();
+	Erzmagier erzmagier=new Erzmagier();
+	Farseer farseer=new Farseer();
+	Daemonenjaeger daemonenjaeger=new Daemonenjaeger();
+	Lich lich =new Lich();
 	
 	Squad(){
-		
+		erstelleSquad();
+	}
+	
+	public void erstelleSquad(){
 		System.out.println("Teamnamen eingeben:");
 		teamName=in.nextLine();
 		
 		System.out.println("Shop:\n"
-							+ "1) Mensch: 110;   2) Erzmagier: 220\n"
-							+ "3) Ork: 150;      4) Farseer: 300\n"
-							+ "5) Nachtelf: 145; 6) Daemonenjaeger: 290\n"
-							+ "7) Untote: 70;    8) Lich: 140");
+							+ "1) Mensch: "+mensch.cost+";   2) Erzmagier: "+erzmagier.cost+"\n"
+							+ "3) Ork: "+ork.cost+";      4) Farseer: "+farseer.cost+"\n"
+							+ "5) Nachtelf: "+nachtelf.cost+"; 6) Daemonenjaeger: "+daemonenjaeger.cost+"\n"
+							+ "7) Untote: "+untote.cost+";    8) Lich: "+lich.cost);
 		
 		while(elfendollar>69 & !fertig){
 			System.out.println("Geld: " + elfendollar);
 			eingabe=in.nextInt();
 			
-			if()
-			switch(eingabe){
-			case 1:	
-					einheiten[zaehler]= new Mensch();
-					
-					break;
-			
-			
+			switch (eingabe) {
+			case 0:
+				fertig = true;
+				break;
+			case 1:
+				if (elfendollar - mensch.cost >= 0) {
+					einheiten[zaehler] = new Mensch();
+					elfendollar -= mensch.cost;
+					zaehler++;
+				} else {
+					System.out.println("Elfendollar reichen nicht aus für diese Rasse");
+				}
+				break;
+			case 2:
+				if (elfendollar - erzmagier.cost >= 0) {
+					einheiten[zaehler] = new Erzmagier();
+					elfendollar -= erzmagier.cost;
+					zaehler++;
+				} else {
+					System.out.println("Elfendollar reichen nicht aus für diese Rasse");
+				}
+				break;
+			case 3:
+				if (elfendollar - ork.cost >= 0) {
+					einheiten[zaehler] = new Ork();
+					elfendollar -= ork.cost;
+					zaehler++;
+				} else {
+					System.out.println("Elfendollar reichen nicht aus für diese Rasse");
+				}
+				break;
+			case 4:
+				if (elfendollar - farseer.cost >= 0) {
+					einheiten[zaehler] = new Farseer();
+					elfendollar -= farseer.cost;
+					zaehler++;
+				} else {
+					System.out.println("Elfendollar reichen nicht aus für diese Rasse");
+				}
+				break;
+			case 5:
+				if (elfendollar - nachtelf.cost >= 0) {
+					einheiten[zaehler] = new Nachtelf();
+					elfendollar -= nachtelf.cost;
+					zaehler++;
+				} else {
+					System.out.println("Elfendollar reichen nicht aus für diese Rasse");
+				}
+				break;
+			case 6:
+				if (elfendollar - daemonenjaeger.cost >= 0) {
+					einheiten[zaehler] = new Daemonenjaeger();
+					elfendollar -= daemonenjaeger.cost;
+					zaehler++;
+				} else {
+					System.out.println("Elfendollar reichen nicht aus für diese Rasse");
+				}
+				break;
+			case 7:
+				if (elfendollar - untote.cost >= 0) {
+					einheiten[zaehler] = new Untote();
+					elfendollar -= untote.cost;
+					zaehler++;
+				} else {
+					System.out.println("Elfendollar reichen nicht aus für diese Rasse");
+				}
+				break;
+			case 8:
+				if (elfendollar - lich.cost >= 0) {
+					einheiten[zaehler] = new Lich();
+					elfendollar -= lich.cost;
+					zaehler++;
+				} else {
+					System.out.println("Elfendollar reichen nicht aus für diese Rasse");
+				}
+				break;
+			default:
+				break;
 			}
-			zaehler++;
+			
 		}
 	}
 	
-	public void attack(){
-		
+	public void attack(Squad verteidiger, Squad angreifer){
+		int zahl;
+		for (int i = 0; i < einheiten.length; i++) {
+			if (angreifer.einheiten[i] != null && angreifer.einheiten[i].isLebendig()){
+				do{
+					zahl =(int)Math.random()*27;
+				} while(!verteidiger.einheiten[zahl].isLebendig());
+						verteidiger.einheiten[zahl].hp-=angreifer.einheiten[i].attack();
+				}
+			if (verteidiger.einheiten[i] != null && verteidiger.einheiten[i].isLebendig()){
+				do{
+					zahl =(int)Math.random()*27;
+				}while(!angreifer.einheiten[zahl].isLebendig());
+						angreifer.einheiten[zahl].hp-=verteidiger.einheiten[i].attack();
+			}	
+		}
 	}
-	
 }
